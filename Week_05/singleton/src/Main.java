fc93add1c989d11df82291ac293fd55b1e4cb76e
yuqiu-pp <41912446@qq.com>
2021-02-09 12:17:18 +0800

    update singlton

--- Week_05/singleton/src/Main.java
+++ Week_05/singleton/src/Main.java
@@ -1,32 +1,30 @@
 import java.lang.reflect.Constructor;
+import java.util.concurrent.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.println("singleton");
         // 反射测试
-        reflectionTest();
+        // try {
+        //     reflectionTest();
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
 
-        hungrySingleTest();
-        lazySingleTest();
         doubleCheckLockSingleTest();
         staticClassSingleTest();
+        hungrySingleTest();
+        lazySingleTest();
         enumSingleTest();
+        ThreadSingleTest();
 
     }
 
-    private static void reflectionTest() throws Exception {
+    private static void doubleCheckLockSingleTest() {
         DoubleCheckLockSingle single01 = DoubleCheckLockSingle.getInstance();
-
-        Class<DoubleCheckLockSingle> clazz = (Class<DoubleCheckLockSingle>) Class.forName("DoubleCheckLockSingle");
-        Constructor<DoubleCheckLockSingle> c = clazz.getDeclaredConstructor();
-        c.setAccessible(true);
-        DoubleCheckLockSingle single03 = c.newInstance();
-        // System.out.println(judgeTwoInstance("reflection", single01, single03));
-    }
-
-    private static void enumSingleTest() {
-        EnumSingle.INSTANCE.doSomething();
+        DoubleCheckLockSingle single02 = DoubleCheckLockSingle.getInstance();
+        System.out.println(judgeTwoInstance("DoubleCheckLock", single01, single02));
     }
 
     private static void staticClassSingleTest() {
@@ -47,10 +45,33 @@ public class Main {
         System.out.println(judgeTwoInstance("LazySingle", single01, single02));
     }
 
-    private static void doubleCheckLockSingleTest() {
-        DoubleCheckLockSingle single01 = DoubleCheckLockSingle.getInstance();
-        DoubleCheckLockSingle single02 = DoubleCheckLockSingle.getInstance();
-        System.out.println(judgeTwoInstance("DoubleCheckLock", single01, single02));
+    private static void enumSingleTest() {
+        EnumSingle.INSTANCE.doSomething();
+    }
+
+    private static void ThreadSingleTest() {
+        // 线程池
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        Future<?> futureA = executorService.submit(() -> {
+            ThreadSingle.getInstance().setValue("A " + Thread.currentThread().getName());
+            return ThreadSingle.getInstance().getValue();
+        });
+
+        // 当线程池数量为1时，task取到的是futureA对应线程中设置的值
+        // 大于1时，取到null， 使用threadlocal时要注意线程池数量
+        FutureTask<String> futureTask = new FutureTask<>(() -> {
+            ThreadSingle.getInstance().setValue("B " + Thread.currentThread().getName());
+            return (String) ThreadSingle.getInstance().getValue();
+        });
+        executorService.submit(futureTask);
+        try {
+            System.out.println(futureA.get());
+            System.out.println(futureTask.get());
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
     }
 
     private static String judgeTwoInstance(String module, Object obj01, Object obj02) {
@@ -60,6 +81,19 @@ public class Main {
             return module + " - 不是同一个";
         }
     }
+
+
+    private static void reflectionTest() throws Exception {
+        DoubleCheckLockSingle single01 = DoubleCheckLockSingle.getInstance();
+
+        Class<DoubleCheckLockSingle> clazz = (Class<DoubleCheckLockSingle>) Class.forName("DoubleCheckLockSingle");
+        Constructor<DoubleCheckLockSingle> c = clazz.getDeclaredConstructor();
+        c.setAccessible(true);
+        DoubleCheckLockSingle single03 = c.newInstance();
+        // System.out.println(judgeTwoInstance("reflection", single01, single03));
+    }
+
+
 
 
 }
--- Week_05/singleton/src/ThreadSingle.java
+++ Week_05/singleton/src/ThreadSingle.java
@@ -2,18 +2,25 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ThreadSingle {
 
-    private static final ConcurrentHashMap<Long, ThreadSingle> instances =
-            new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<Long, ThreadSingle> instances = new ConcurrentHashMap<>();
+
+    private Object value = null;
 
     private ThreadSingle() {}
 
     // 因为是不同线程的，所以这里不需要 锁
     public static ThreadSingle getInstance() {
         Long threadId = Thread.currentThread().getId();
-        // if (!instances.contains(threadId)) {
-        //     instances.put(threadId, new ThreadSingle());
-        // }
+        // 每个线程 拥有一个single实例
         instances.putIfAbsent(threadId, new ThreadSingle());
         return instances.get(threadId);
+    }
+
+    public void setValue(Object obj) {
+        this.value = obj;
+    }
+
+    public Object getValue() {
+        return this.value;
     }
 }
